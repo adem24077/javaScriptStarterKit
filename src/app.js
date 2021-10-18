2d402fc70c4d585db775dc74d6d94dbbebb5fb85
Adem Furkan Kavuş <150111027@ogrenci.yalova.edu.tr>
2021-10-19 00:02:37 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,35 @@
+console.log("Merhaba Kodlama.io")
+//JS type safe değil.
+let dolarBugun = 9.30
+
+let dolarDun= 9.20
+dolarDun= "9.20"
+{   
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCase
+let konutKredileri = ["Konut kredisi", 
+"Emlak Konut Kredisi",
+"Kamu Konut Kredisi","Özel Konut Kredileri"]
+
+console.log("<ul>")
+
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul>")
+
+
+console.log(konutKredileri)
+
